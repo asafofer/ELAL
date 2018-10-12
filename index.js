7e89b21fef45e2d580a6daa69245ed42be95ff81
asafofer <asafofer95@gmail.com>
2018-10-12 16:39:35 +0300

    first

---
+++ index.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+app.use('/assets', express.static('assets'));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
+
+app.get('*', (req, res) => res.redirect('/'));
+
+const PORT = process.env.PORT || 1337;
+app.listen(PORT, () => {
+  console.log('up and running');
+});
